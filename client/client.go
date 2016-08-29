06c7a4cb7b81171c44a66dd2b7ed453de64ac5c6
Joakim Gustin <joakim.gustin@collectorbank.se>
2016-08-29 15:00:48 +0200

    Enable mutual TLS

--- client/client.go
+++ client/client.go
@@ -1,25 +1,57 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"flag"
+	"io/ioutil"
 	"log"
+	"os"
+	"path/filepath"
 	"time"
 
 	"golang.org/x/net/context"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 
 	pb "github.com/gust1n/randomNumberService/randomnumber"
 )
 
-var address = flag.String("addr", "localhost:9000", "Server address")
+func withConfigDir(path string) string {
+	return filepath.Join(os.Getenv("HOME"), ".randomNumber", "client", path)
+}
 
 func main() {
+	var (
+		caCert     = flag.String("ca-cert", withConfigDir("ca.pem"), "Trusted CA certificate.")
+		serverAddr = flag.String("server-addr", "127.0.0.1:9000", "Hello service address.")
+		tlsCert    = flag.String("tls-cert", withConfigDir("cert.pem"), "TLS server certificate.")
+		tlsKey     = flag.String("tls-key", withConfigDir("key.pem"), "TLS server key.")
+	)
 	flag.Parse()
 
-	conn, err := grpc.Dial(*address, grpc.WithInsecure())
+	cert, err := tls.LoadX509KeyPair(*tlsCert, *tlsKey)
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		log.Fatal(err)
+	}
+
+	rawCACert, err := ioutil.ReadFile(*caCert)
+	if err != nil {
+		log.Fatal(err)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(rawCACert)
+
+	creds := credentials.NewTLS(&tls.Config{
+		Certificates: []tls.Certificate{cert},
+		RootCAs:      caCertPool,
+	})
+
+	conn, err := grpc.Dial(*serverAddr,
+		grpc.WithTransportCredentials(creds))
+	if err != nil {
+		log.Fatal(err)
 	}
 	defer conn.Close()
 
--- server/server.go
+++ server/server.go
@@ -1,20 +1,23 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"flag"
-	"fmt"
+	"io/ioutil"
 	"log"
 	"math/rand"
 	"net"
+	"os"
+	"path/filepath"
 
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
 
 	pb "github.com/gust1n/randomNumberService/randomnumber"
 )
-
-var port = flag.Int("port", 9000, "Port to serve on")
 
 type randomNumberServer struct{}
 
@@ -27,13 +30,47 @@ func (s *randomNumberServer) GetInt(ctx context.Context, req *pb.GetIntRequest) 
 
 }
 
+func withConfigDir(path string) string {
+	return filepath.Join(os.Getenv("HOME"), ".randomNumber", "server", path)
+}
+
 func main() {
+	var (
+		caCert     = flag.String("ca-cert", withConfigDir("ca.pem"), "Trusted CA certificate.")
+		listenAddr = flag.String("listen-addr", "0.0.0.0:9000", "HTTP listen address.")
+		tlsCert    = flag.String("tls-cert", withConfigDir("cert.pem"), "TLS server certificate.")
+		tlsKey     = flag.String("tls-key", withConfigDir("key.pem"), "TLS server key.")
+	)
 	flag.Parse()
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+
+	log.Println("Random number service starting...")
+
+	cert, err := tls.LoadX509KeyPair(*tlsCert, *tlsKey)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatal(err)
 	}
-	grpcServer := grpc.NewServer()
-	pb.RegisterRandomNumberServer(grpcServer, &randomNumberServer{})
-	grpcServer.Serve(lis)
+
+	rawCaCert, err := ioutil.ReadFile(*caCert)
+	if err != nil {
+		log.Fatal(err)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(rawCaCert)
+
+	creds := credentials.NewTLS(&tls.Config{
+		Certificates: []tls.Certificate{cert},
+		ClientCAs:    caCertPool,
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+	})
+
+	gs := grpc.NewServer(grpc.Creds(creds))
+	pb.RegisterRandomNumberServer(gs, &randomNumberServer{})
+
+	lis, err := net.Listen("tcp", *listenAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Random service started successfully")
+	log.Fatal(gs.Serve(lis))
 }
